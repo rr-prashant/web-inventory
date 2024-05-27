09f8024619255806315d6b41f3724a259e218e32
prashant <rumdali.psn@gmail.com>
2024-05-27 15:06:12 +0545

    final commit

--- js3-SlideForm/script.js
+++ js3-SlideForm/script.js
@@ -16,7 +16,15 @@ function displayTab(t){
     }
 
     // display only the current tab
-    ctab[t].style.display = "block";
+    if (tabCount < ctab.length){
+        ctab[t].style.display = "block";
+    }
+    else{
+        let form = document.getElementById("formdata");
+        form.style.display = "none";
+    }
+    
+    
 
     
     // buttons integration respectively with tabs
@@ -36,9 +44,12 @@ function displayTab(t){
 }
 
 nextBtn.addEventListener('click', (e)=>{
+    var ctab = document.getElementsByClassName("screenTab");
     e.preventDefault();
     tabCount++;
     displayTab(tabCount);
+    
+    
 })
 
 prevBtn.addEventListener('click', (e)=>{
@@ -60,12 +71,15 @@ function actionButton(){
     var tabs = document.getElementsByClassName("screenTab");
     if ( tabCount == 0 ){
         userinfo.name = name;
-    }else if (tabCount == ( tabs.length - 1)){
+    }else if ( tabCount == (tabs.length - 1)){
         userinfo.comment = message;
-        submitAction();
     }else{
         userinfo.email = email;
-        userinfo.subscription = "Subscribed";
+        if (sub){
+            userinfo.subscription = "Subscribed";
+        }else{
+            userinfo.subscription = "Not subscribed";
+        }
         if (male){
             userinfo.gender = "male";
 
@@ -73,14 +87,53 @@ function actionButton(){
         else{
             userinfo.gender = "female";
         }
+        console.log(tabs.length);
     }
 
     const objToString = JSON.stringify(userinfo);
     localStorage.setItem("userDetails", objToString);
+
+    
+    nextBtn.addEventListener('click', (e)=>{
+        if (tabCount > (tabs.length - 1)){
+            submitAction(tabCount);
+        }
+    })
+
 }
 
-// function submitAction(){
-//     var new_obj = JSON.parse(localStorage.getItem("userDetails"));
+function submitAction(n){
+    let new_obj = JSON.parse(localStorage.getItem("userDetails"));
 
-//     alert()
-// }
+    let tag = document.getElementById("resultPrompt");
+    let content = document.getElementById("submitcontent");
+    var tabs = document.getElementsByClassName("screenTab");
+    let form = document.getElementById("formdata");
+
+    content.innerHTML = `
+    Name: ${new_obj.name}<br>
+    Email: ${new_obj.email}<br>
+    Gender: ${new_obj.gender}<br>
+    Subscription: ${new_obj.subscription}<br>
+    Message: ${new_obj.comment}
+    `;
+
+    console.log(new_obj.email);
+
+    tag.style.display = "block";
+
+    tabs[n].style.display = "none";
+    form.style.display = "none";
+    
+    
+}
+
+let closebtn = document.getElementById("close");
+
+closebtn.addEventListener('click', (e)=>{
+    closeAction();
+})
+
+function closeAction(){
+    window.location.href = './index.html'
+}
